36f69ecc96d8b403d19e35a3e72f8078267bf799
Anil Deshpande <anildesh82@gmail.com>
2017-08-15 11:10:08 +0530

    This is the improved code with holder design pattern.

--- app/src/main/java/codetutor/com/recyclelistveiwdemo/ListAdapterPeople.java
+++ app/src/main/java/codetutor/com/recyclelistveiwdemo/ListAdapterPeople.java
@@ -45,18 +45,40 @@ public class ListAdapterPeople extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
+        PersonViewHolder personViewHolder;
 
-        layoutInflater = LayoutInflater.from(this.context);
+        if(view==null){
+            layoutInflater = LayoutInflater.from(this.context);
+
+            view=layoutInflater.inflate(R.layout.layout_person_row_item,null);
+            personViewHolder=new PersonViewHolder();
+
+            personViewHolder.textViewName = (TextView)view.findViewById(R.id.textViewName);
+            personViewHolder.textViewLastName=(TextView)view.findViewById(R.id.textViewLastName);
+            personViewHolder.textViewGender = (TextView)view.findViewById(R.id.textViewGender);
+            personViewHolder.textViewNationality = (TextView)view.findViewById(R.id.textViewNationality);
+
+            view.setTag(personViewHolder);
+        }else{
+            personViewHolder = (PersonViewHolder)view.getTag();
+        }
 
         final Person person = people.get(i);
 
-        view=layoutInflater.inflate(R.layout.layout_person_row_item,null);
-        ((TextView)view.findViewById(R.id.textViewName)).setText(person.getName());
-        ((TextView)view.findViewById(R.id.textViewLastName)).setText(person.getLastName());
-        ((TextView)view.findViewById(R.id.textViewGender)).setText((person.getGender()== Person.GENDER.MALE? "Male":"Female"));
-        ((TextView)view.findViewById(R.id.textViewNationality)).setText(person.getNationality());
+        personViewHolder.textViewName.setText(person.getName());
+        personViewHolder.textViewLastName.setText(person.getLastName());
+        personViewHolder.textViewNationality.setText(person.getNationality());
+        personViewHolder.textViewGender.setText((person.getGender()== Person.GENDER.MALE? "Male":"Female"));
 
         Log.i(TAG,"Index: "+i+" : "+view);
+
         return view;
+    }
+
+    private static class PersonViewHolder{
+        public TextView textViewName;
+        public TextView textViewLastName;
+        public TextView textViewGender;
+        public TextView textViewNationality;
     }
 }
